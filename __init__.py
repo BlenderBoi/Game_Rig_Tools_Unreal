0237c78d7a5136ceb2a5741c2b9c9b078126770c
blenderboi <blenderboi2018@gmail.com>
2024-07-06 06:26:38 +0000

    Added Tracker Link, Updated Documentation Link, Fixed Pre Extracted mannequin System UE5 (By CG Dive)

--- __init__.py
+++ __init__.py
@@ -1,12 +1,12 @@
-
 bl_info = {
     "name": "Game Rig Tools - Unreal Module",
     "author": "BlenderBoi",
-    "version": (4, 0, 1),
+    "version": (4, 1, 1),
     "blender": (4, 0, 0),
     "description": "Game Rig Tools With Tools Caters to Unreal Workflow",
     "warning": "",
-    "doc_url": "https://tarry-dill-523.notion.site/Game-Rig-Tools-Documentation-dc61839fd1d04147a42aa89e366575fb",
+    "doc_url": "https://cgdive.notion.site/Game-Rig-Tools-Documentation-dc61839fd1d04147a42aa89e366575fb",
+    "tracker_url": "https://support.tinkerboi.com/t/bug-report",
     "category": "Armature",
 }
 
@@ -16,19 +16,18 @@ from . import Preferences
 from . import Property
 
 
-
 modules = [Property, Operators, Preferences]
 
 
 def register():
-
     for module in modules:
         module.register()
 
-def unregister():
 
+def unregister():
     for module in modules:
         module.unregister()
+
 
 if __name__ == "__main__":
     register()
